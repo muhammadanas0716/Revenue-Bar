f4f8b3b12c1301365e830eddcfe0134e38cd6360
muhammadanas0716 <iam.muhammadanas0716@gmail.com>
2026-01-13 08:09:34 +0400

    Initial Commit

---
+++ MRR Bar/MRR_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRR_BarApp.swift
+//  MRR Bar
+//
+//  Created by Muhammad Anas on 13/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MRR_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
